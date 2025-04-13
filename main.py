ce5afd83babce3d4dec3d16312ea231191d659ce
bhaskarbidyanta <bhaskar.bid18@gmail.com>
2025-04-13 14:15:30 +0530

    Data load and caching

--- main.py
+++ main.py
@@ -14,7 +14,20 @@ TODAY = date.today().strftime("%Y-%m-%d")
 st.title("Stock Prediction App")
 
 stocks = ("RELIANCE.NS", "TATAMOTORS.NS", "HDFCBANK.NS", "ICICIBANK.NS", "INFY.NS", "TCS.NS", "HINDUNILVR.NS", "LT.NS", "HDFC.NS", "SBIN.NS")
-selected_stocks = st.selectbox("Select dataset for prediction", stocks)
+selected_stock = st.selectbox("Select dataset for prediction", stocks)
 
 n_years = st.slider("Years of prediction:",1, 4)
 period = n_years * 365
+
+@st.cache
+def load_data(ticker):
+    data = yf.download(ticker,START,TODAY)
+    data.reset_index(inplace=True)
+    return data
+
+data_load_state = st.text("Load data...")
+data = load_data(selected_stock)
+data_load_state.text("Loading data... done!")
+
+
+
