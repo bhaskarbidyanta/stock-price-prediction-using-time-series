4fa51edc6d7e8be034c04c6dd6e079e3f3c08d47
bhaskarbidyanta <bhaskar.bid18@gmail.com>
2025-04-13 14:09:02 +0530

    Updated main.py with basic functionalities

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from datetime import datetime
+from datetime import date
 
 import yfinance as yf
 from prophet import Prophet
@@ -8,3 +8,13 @@ from plotly import graph_objs as go
 import pandas as pd
 import numpy as np
 
+START = "2015-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
+
+st.title("Stock Prediction App")
+
+stocks = ("RELIANCE.NS", "TATAMOTORS.NS", "HDFCBANK.NS", "ICICIBANK.NS", "INFY.NS", "TCS.NS", "HINDUNILVR.NS", "LT.NS", "HDFC.NS", "SBIN.NS")
+selected_stocks = st.selectbox("Select dataset for prediction", stocks)
+
+n_years = st.slider("Years of prediction:",1, 4)
+period = n_years * 365
